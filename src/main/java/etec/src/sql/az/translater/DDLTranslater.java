a49320adc14ec04012bc0e84e6087005968c0730
Tim <timyug@gmail.com>
2024-06-20 11:16:40 +0800

    AZ
    CTAS優化

--- src/main/java/etec/src/sql/az/translater/DDLTranslater.java
+++ src/main/java/etec/src/sql/az/translater/DDLTranslater.java
@@ -14,9 +14,7 @@ public class DDLTranslater {
 	public static String easyReplace(String title,String sql) throws SQLTranslateException {
 		String res = sql;
 		if(sql.matches("(?i)\\s*CREATE(?:\\s+(?:TEMP|SET|MULTISET))?\\s+[\\S\\s]+")) {
-			res = replaceCreateTitle(res);
-			res = replaceTDsql(res);
-			res = replaceColumn(res);
+			
 			if(sql.matches("(?i)\\s*Create\\s+Table\\s+\\S+\\s+As\\s*[\\S\\s]+")) {
 				Log.debug("\t\t細分：CTAS");
 				sql = runCTAS(sql);
@@ -39,6 +37,8 @@ public class DDLTranslater {
 	public static String runCreateTable(String sql) throws SQLTranslateException {
 		// create語法轉換
 		String res = sql;
+		res = replaceCreateTitle(res);
+		res = replaceColumn(res);
 		// Index語法轉換
 		res = addWith(res);
 		// drop if exist
@@ -46,47 +46,70 @@ public class DDLTranslater {
 		res = drop + res;
 		return res;
 	}
-	// CTAS
+	
+	/**
+	 * <h1>CTAS</h1>
+	 * <p>
+	 * <br>先清除註解並處理index
+	 * <br>拆開sub query 
+	 * <br>加上 if not exists
+	 * </p>
+	 * <p></p>
+	 * 
+	 * <h2>異動紀錄</h2>
+	 * <br>2024年6月20日	Tim	建立功能
+	 * 
+	 * @author	Tim
+	 * @since	1.0.0.0
+	 * @param	enclosing_method_arguments
+	 * @throws	e
+	 * @see		
+	 * @return	return_type
+			 */
 	public static String runCTAS(String sql) throws SQLTranslateException {
-		String res = "";
-		String create = "";
-		String select = "";
-		String with = "";
-		// create
-		create = RegexTool.getRegexTarget("(?i)CREATE\\s+TABLE\\s+\\S+", 
-					sql.replaceAll("(?i)\\bVOLATILE\\b", " ")
-					.replaceAll("(?i)\\bMULTISET\\b", "")
-					.replaceAll("(?i)\\bSET\\s+TABLE", "TABLE")
-				).get(0)
-				;
-		// with
-		List<String> lstPrimaryIndex = RegexTool.getRegexTarget("(?i)UNIQUE\\s+PRIMARY\\s+INDEX\\s+\\([^\\)]+\\)", sql);
-		List<String> lstIndex = RegexTool.getRegexTarget("(?i)PRIMARY\\s+INDEX\\s+\\([^\\)]+\\)", sql);
-		String column = "";
-		if (!lstPrimaryIndex.isEmpty()) {
-			String indexCol = lstPrimaryIndex.get(0).replaceAll("(?i)UNIQUE\\s+PRIMARY\\s+INDEX\\s+\\(", "")
-					.replaceAll("\\)", "").replaceAll("\\s", "").trim();
-			column += indexCol;
+		String res = sql;
+		
+		res = replaceCreateTitle(res);
+		res = addWith(res);
+		
+		/**
+		 * <p>功能 ：CTAS拆解</p>
+		 * <p>類型 ：搜尋</p>
+		 * <p>修飾詞：is</p>
+		 * <p>範圍 ：從  create 到 with</p>
+		 * <h2>群組 ：</h2>
+		 * 	1.setting 	: VOLATILE,MULTISET...等修飾詞
+		 *  2.tableName : 如題
+		 *  3.select	: CTAS的sub query
+		 * <h2>備註 ：</h2>
+		 * <p>
+		 * </p>
+		 * <h2>異動紀錄 ：</h2>
+		 * 2024年6月20日	Tim	建立邏輯
+		 * */
+		StringBuffer sb = new StringBuffer();
+		String reg = "(?is)"
+				+ "CREATE\\s+(?<setting>.*?)\\s+"
+				+ "TABLE\\s+(?<tableName>[^,\\s()]+)\\s+"
+				+ "AS\\s*\\(\\s*(?<select>SELECT.*)?"
+				+ "\\)(?=.*WITH\\s*\\()";
+		Matcher m = Pattern.compile(reg).matcher(res);
+		while (m.find()) {
+			String newSql = "";
+			String setting = m.group("setting");
+			String tableName = m.group("tableName");
+			String select = m.group("select");
+			
+			select = DQLTranslater.easyReplace(select);
+			
+			
+			newSql = "CREATE TABLE IF NOT EXISTS "+tableName + " (\r\n"
+					+ select+"\r\n)";
+			
+			m.appendReplacement(sb, Matcher.quoteReplacement(newSql));
 		}
-		if (!lstIndex.isEmpty()) {
-			if (!lstPrimaryIndex.isEmpty()) {
-				column += ",";
-			}
-			String indexCol = lstIndex.get(0).replaceAll("(?i)PRIMARY\\s+INDEX\\s+\\(", "").replaceAll("\\)", "")
-					.replaceAll("\\s", "").trim();
-			column += indexCol;
-		}
-		String hash = "DISTRIBUTION = " + ("".equals(column) ? "REPLICATE" : "HASH(" + column + ")");
-		with = "WITH (" + "\r\n\tCLUSTERED COLUMNSTORE INDEX," + "\r\n\t" + hash + "\r\n)";
-		// select
-		String oldselect = sql
-				.replaceAll("(?i)CREATE(\\s+\\S+)*\\s+TABLE\\s+\\S+\\s+AS\\s*\\(", "")
-				.replaceAll("(?i)\\)\\s*WITH\\s+DATA\\s*[^;]+", "")
-//				.replaceAll("TtEeSsTt", "%;%")
-				.trim();
-		select = DQLTranslater.easyReplace(oldselect);
-		res = create.trim() + "\r\n" + with.trim() + "\r\nAS\r\n" + select.trim()+"\r\n;";
-		res = res.replaceAll(";\\s*;", ";");
+		m.appendTail(sb);
+		res = sb.toString();
 		return res;
 	}
 	/**
@@ -137,19 +160,22 @@ public class DDLTranslater {
 	}
 	
 	// 轉換create table
-	private static String replaceCreateTitle(String sql) {
+	public static String replaceCreateTitle(String sql) {
 		String result = sql.replaceAll("(?i)\\s*,\\s*NO\\s*FALLBACK\\s*", " ")
 				.replaceAll("(?i)\\s*,\\s*NO\\s*[A-Za-z]+\\s*JOURNAL\\s*", " ")
 				.replaceAll("(?i)\\s*,\\s*CHECKSUM\\s*=\\s*[A-Za-z]+\\s*", " ")
 				.replaceAll("(?i)\\s*,\\s*DEFAULT\\s*MERGEBLOCKRATIO\\s*", " ")
 				.replaceAll("(?i)CHARACTER\\s+SET\\s+\\S+", " ")
 				.replaceAll("(?i)(NOT\\s+)?CASESPECIFIC", " ")
+				.replaceAll("(?i)\\bON\\s+COMMIT\\b", "") // on commit
+				.replaceAll("(?i)\\bPRESERVE\\s+ROWS\\b","")//PRESERVE ROWS
 				.replaceAll("(?i)TITLE\\s+'[^']+'", " ");
 		return result;
 	}
 	/**
 	 * <h1>with處理</h1>
 	 * <p>
+	 * <br>不一定要整段SQL，可以只提供 最後段的設定
 	 * <br>首先要清除所有index
 	 * <br>在SQL尾端加上with語法
 	 * <br>如果原本有INDEX,要用HASH
@@ -171,7 +197,7 @@ public class DDLTranslater {
 	 * @see		
 	 * @return	return_type
 			 */
-	private static String addWith(String sql) {
+	public static String addWith(String sql) {
 		String result = sql;
 		/**
 		 * <p>功能 ：取得index</p>
@@ -198,11 +224,12 @@ public class DDLTranslater {
 				+ "\r\n\tCLUSTERED COLUMNSTORE INDEX,"
 				+ "\r\n\tDISTRIBUTION = "
 				+ (lst.isEmpty()?"REPLICATE":"HASH( " + String.join(",", lst) + " )")
-				+ "\r\n)"
+				+ "\r\n)\r\n;"
 				;
-		sb.append(distribution);
+//		sb.append(distribution);
 		m.appendTail(sb);
-		result = sb.toString();
+		
+		result = sb.toString().replaceAll("\\s*;\\s*$","") + distribution;
 //		
 //		// 取得欄位
 //		List<String> lstPrimaryIndex = RegexTool.getRegexTarget("(?i)UNIQUE\\s+PRIMARY\\s+INDEX\\s+\\([^\\)]+\\)", temp);
@@ -227,8 +254,11 @@ public class DDLTranslater {
 //		result += hash + "\r\n)";
 		return result;
 	}
+	
 	// 清除TD特有的語法
-	private static String replaceTDsql(String sql) {
+	@Deprecated
+	public
+	static String replaceTDsql(String sql) {
 		String result = sql
 				.replaceAll("(?i)\\bVOLATILE\\b", "")// VOLATILE
 				.replaceAll("(?i)\\bMULTISET\\b", "")// MULTISET
@@ -237,13 +267,12 @@ public class DDLTranslater {
 				.replaceAll("(?i)NO\\s+PRIMARY\\s+INDEX", "")
 				.replaceAll("(?i)RANGE_N" + "\\s*\\([^\\)]+\\)", " ")// PARTITION BY
 				.replaceAll("(?i)PARTITION\\s+BY\\s*(\\s*\\([^\\)]*\\))?", " ")// PARTITION BY
-				.replaceAll("(?i)\\bON\\s+COMMIT\\b", "") // on commit
-				.replaceAll("(?i)\\bPRESERVE\\s+ROWS\\b","")//PRESERVE ROWS
 		;
 		return result;
 	}
+	
 	// 清除欄位的多餘設定
-	private static String replaceColumn(String sql) {
+	public static String replaceColumn(String sql) {
 		String result = sql.replaceAll("(?i)CHARACTER\\s+SET\\s+\\S+", " ")
 				.replaceAll("(?i)NOT\\s+CASESPECIFIC", " ")
 				.replaceAll("(?i)TITLE\\s+'[^']+'", " ")
@@ -253,6 +282,7 @@ public class DDLTranslater {
 				.replaceAll(" +,", ",");
 		return result;
 	}
+	
 	/**
 	 * @author	Tim
 	 * @since	2023年11月17日
