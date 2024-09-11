29e21ee03b80cee9f0da6364dc573ecd0dcee1bf
tim040116 <timyug@gmail.com>
2024-09-11 16:55:33 +0800

    az
    兩個rollup功能完成

--- src/main/java/etec/src/translator/sql/az/translater/DQLTranslater.java
+++ src/main/java/etec/src/translator/sql/az/translater/DQLTranslater.java
@@ -1,5 +1,6 @@
 package etec.src.translator.sql.az.translater;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -161,12 +162,31 @@ public class DQLTranslater {
 			 */
 	public static String changeRollUp(String sql) {
 		String res = sql;
+		//整理語法
 		res = res.replaceAll("(?i),\\s*ROLLUP\\s*\\(",",ROLLUP\\(");
+		//將rollup排序到最後
 		res = res.replaceAll("(?i)(,ROLLUP\\([^)]+\\))\\s*((?:,\\s*[\\w.]+\\s*)+)","$2$1");
 		res = res.replaceAll("(?i)GROUP\\s+BY\\s+(ROLLUP\\([^)]+\\))\\s*,\\s*([\\w.]+\\s*(?:,\\s*[\\w.]+\\s*)+)(,ROLLUP\\([^);]+\\))","GROUP BY $2,$1$3");
 		res = res.replaceAll("(?i)GROUP\\s+BY\\s+(ROLLUP\\([^)]+\\))\\s*,\\s*([\\w.]+\\s*(?:,\\s*[\\w.]+\\s*)+)","GROUP BY $2,$1");
+		//將複數rollup拆分成union
+		res = multiRollup(res);
+		//轉換rollup
+		res = singleRollup(res);
+		//整理having
+		res = res.replaceAll("(?i)(\\bhaving\\s+\\S+\\s*is not null(?:\\s*and\\s*\\S+\\s+is not null)+\\s*)having\\b", "$1   and");
+		return res;
+	}
+	
+	//單一rollup轉換
+	private static String singleRollup(String scrippt) {
+		//轉換rollup
 		StringBuffer sb = new StringBuffer();
-		Matcher m = Pattern.compile("(?i)([ \\t]*)GROUP\\s+BY\\s+([\\w.,\\s]+),ROLLUP\\s*\\(([^)]+)\\)(\\s*,\\s*ROLLUP\\s*\\([^)]+\\))?").matcher(res);
+		String reg = "(?i)([ \\t]*)"
+			+ "GROUP\\s+BY\\s+([\\w.,\\s]+)"
+			+ ",ROLLUP\\s*\\(([^)]+)\\)"
+			+ "(\\s*,\\s*ROLLUP\\s*\\([^)]+\\))?"
+		;
+		Matcher m= Pattern.compile(reg).matcher(scrippt);
 		while(m.find()) {
 			String tab = m.group(1);
 			String groupby = m.group(2).replaceAll("\\s+", "");
@@ -178,6 +198,66 @@ public class DQLTranslater {
 				+ "\r\n" + tab + having + "\r\n"
 			;
 			m.appendReplacement(sb, rpm);
+		}
+		m.appendTail(sb);
+		return sb.toString();
+	}
+	//將複數rollup拆分成union
+	private static String multiRollup(String script) {
+		StringBuffer sb = new StringBuffer();
+		String reg = "(?i)(?<head>^.*\\n"
+				+ "(?<tab>[ \\t]+)GROUP\\s+BY[\\w.,\\s]+"
+				+ "ROLLUP\\()(?<rollup1>[^)]+)\\)\\s*"
+				+ "(,\\s*ROLLUP\\((?<rollup2>[^)]+)\\))"
+				+ "(?<foot>.*$)";
+		Matcher m= Pattern.compile(reg,Pattern.DOTALL).matcher(script);
+		while(m.find()) {
+			List<String> lst = new ArrayList<String>();
+			//取出參數
+			String head = m.group("head");
+			String tab = m.group("tab");
+			String rollup1 = m.group("rollup1");
+			String rollup2 = m.group("rollup2");
+			String foot = m.group("foot");
+			//加工
+			String tmpHaving = "";
+			String tmpRollup2 = "";
+			String[] arrRollup2 = rollup2.trim().split("\\s*,\\s*");
+			//先製作最下層的語法
+			String tmpFirstHead = head;
+			for(String rplCol : arrRollup2) {
+				tmpFirstHead = tmpFirstHead.replaceAll(Pattern.quote(rplCol), "null");
+			}
+			String rpm = tmpFirstHead
+				+ rollup1 + ")"
+				+ tab + foot
+			;
+			//迴圈疊union
+			for(String col : arrRollup2) {
+				lst.add(col);
+				tmpRollup2 += col + ",";
+				tmpHaving += 
+						(tmpHaving.equals("")?tmpHaving+"having ":tmpHaving+"   and")
+						+ col + " is not null\r\n"
+				;
+				String tmpHead = head;
+				for(String rplCol : arrRollup2) {
+					if(!lst.contains(rplCol)) {
+						tmpHead = tmpHead.replaceAll(Pattern.quote(rplCol), "null");
+					}
+				}
+				String tmpsql = tmpHead
+					+ tmpRollup2 + rollup1 + ")"
+					+ tab + tmpHaving
+					+ tab + foot
+					+ "\r\n"
+				;
+				rpm = tmpsql 
+					+ "\r\nUNION"
+					+ "\r\n" + rpm
+				;
+			}
+			m.appendReplacement(sb,Matcher.quoteReplacement(rpm));	
 		}
 		m.appendTail(sb);
 		return sb.toString();
